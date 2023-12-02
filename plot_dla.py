48159aca7cfc0d097797bd62694471658c918890
natkachov <n.a.tkachov@yandex.ru>
2023-12-02 19:01:58 +0300

    plot in 3d and slice

---
+++ plot_dla.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+from matplotlib.pyplot import plot, show, draw, figure, cm
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+EDGE_FIELD = 20
+df = pd.read_csv(f"/home/natkachov/datasets/export_from_dla_cuda/dla_cuda_{EDGE_FIELD}.csv")
+# df.reset_index(drop=True)
+print(df)
+df.head()
+df['status_point'] = df['property'].astype(int)
+df = df[df['property'] == 1.0]
+print(df)
+print(len(df))
+df.head()
+x = df['x']
+y = df['y']
+z = df['z']
+
+df_3d = df[['x', 'y', 'z', 'status_point']]
+color_points = df['status_point']
+
+# Create a 3D scatter plot with Seaborn
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y, z, c=color_points, cmap='viridis', marker='o')
+ax.set_xlabel('X-axis')
+ax.set_ylabel('Y-axis')
+ax.set_zlabel('Z-axis')
+plt.title('3D Scatter Plot with Seaborn')
+plt.show()
+
+sns.pairplot(df_3d, hue='status_point')
+plt.show()
