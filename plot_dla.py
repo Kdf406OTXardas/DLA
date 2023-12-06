1d1288df2c1b1225ebc1901de5b28dd5bec1e5e3
natkachov <n.a.tkachov@yandex.ru>
2023-12-06 21:22:12 +0300

    plotting

--- plot_dla.py
+++ plot_dla.py
@@ -5,8 +5,10 @@ from matplotlib.pyplot import plot, show, draw, figure, cm
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-EDGE_FIELD = 20
-df = pd.read_csv(f"/home/natkachov/datasets/export_from_dla_cuda/dla_cuda_{EDGE_FIELD}.csv")
+EDGE_FIELD = 60
+END_POROSITY = 3
+
+df = pd.read_csv(f"/home/natkachov/datasets/export_from_dla_cuda/dla_cuda_{EDGE_FIELD}_{100 - END_POROSITY}.csv")
 # df.reset_index(drop=True)
 print(df)
 df.head()
@@ -23,13 +25,13 @@ df_3d = df[['x', 'y', 'z', 'status_point']]
 color_points = df['status_point']
 
 # Create a 3D scatter plot with Seaborn
-fig = plt.figure(figsize=(8, 6))
+fig = plt.figure(figsize=(12, 12))
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(x, y, z, c=color_points, cmap='viridis', marker='o')
 ax.set_xlabel('X-axis')
 ax.set_ylabel('Y-axis')
 ax.set_zlabel('Z-axis')
-plt.title('3D Scatter Plot with Seaborn')
+plt.title('DLA')
 plt.show()
 
 sns.pairplot(df_3d, hue='status_point')
